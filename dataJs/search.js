9aa7c8dc1eec63ea8516294a23ed4f29ed3b8e71
slh <slh>
2017-03-06 13:58:05 +0800

    origin change

--- dataJs/search.js
+++ dataJs/search.js
@@ -172,7 +172,7 @@ $(document).ready(function () {
                 myViewer.viewer();
 
 
-                $("#li_origin").append("<img src="+data["origin_immg"]+"'>");
+                $("#li_origin").append("<img style='max-width: 100%' id='imagei' src="+data['origin_immg']+">");
             },
             error:function(xhr,textStatus){
                 console.log('错误');
