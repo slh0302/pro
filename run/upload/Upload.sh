aa76ea46f70e8a4977a8e52b03af140251d72c5e
slh <slh>
2017-03-06 18:05:42 +0800

    upload change

---
+++ run/upload/Upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# DoIndex -i: Create database
+file_list_path=$1
+pic_src_path=$2
+pic_list_count=$3
+result_database_path=$4
+cd /home/slh/Vechile_Search/libfaster_rcnn_cpp/
+./bin/DoIndex -i $file_list_path $pic_src_path $pic_list_count 250 $result_database_path GPU 0
+
+
+
+
