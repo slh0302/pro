9acfe64c5b50040e841c408bbbf12d589a2f268f
slh <slh>
2017-03-06 10:56:57 +0800

    server change

--- run/search/DoSearch.sh
+++ run/search/DoSearch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/slh/Vechile_Search/libfaster_rcnn_cpp
-./bin/DoIndex -c /home/slh/shuju1080000 1080000  $1 1 1 128 GPU 0
+./bin/DoIndex -c /home/slh/data_model_1200_1080000 1080000  $1 1 1 128 GPU 0
 
 
 #/home/saltedfish/runtest  $1
