db43629ef1f214ce91cb0c7c0da0f004654c827a
slh <slh@bupt.edu.cn>
2017-03-10 20:55:18 +0800

    upload final

--- run/upload/Upload.sh
+++ run/upload/Upload.sh
@@ -6,8 +6,7 @@ pic_src_path=$2
 pic_list_count=$3
 result_database_path=$4
 cd /home/slh/Vechile_Search/libfaster_rcnn_cpp/
+$te="./bin/DoIndex -i $file_list_path $pic_src_path $pic_list_count 250 $result_database_path GPU 0"
+echo $te
 ./bin/DoIndex -i $file_list_path $pic_src_path $pic_list_count 250 $result_database_path GPU 0
-
-
-
 
--- run/upload/linux/LoadToDatabase.cpp
+++ run/upload/linux/LoadToDatabase.cpp
@@ -7,6 +7,8 @@ using namespace std;
 //#pragma comment(lib,"libmysql.lib")
 int main(int strs, char** str) {
 	string databasename = str[2];
+	string filePath=str[1];
+	string fileout=filePath+"_out";
 	//mysql init 
 	MYSQL mysql;
 	MYSQL_RES *result;
@@ -15,17 +17,19 @@ int main(int strs, char** str) {
 	mysql_real_connect(&mysql, "localhost", "root", "root", "test", 3306, NULL, 0);
 	
 	string sql = "Insert into " + databasename + "(name,Location) values ('";
-
 	ifstream infile(str[1], ios::in);
+	ofstream outfile(fileout.c_str(),ios::out);
 	string location;
 	long long num = 0;
+	string filepath;
 	while (infile>>location) {
 		num++;
 		int index=location.find_last_of('/');
-		string filepath = location.substr(0, index);
+		filepath = location.substr(0, index);
 		string filename = location.substr(index + 1);
-		
+		outfile<<filename<< " "<<"0"<<endl;
 		string sql_query = sql + filename + "' , '" + filepath + "');";
+//		cout<<sql_query<<endl;
 		mysql_query(&mysql, sql_query.c_str());
 		//cout << filepath << "   " << filename << endl;
 	}
@@ -34,6 +38,6 @@ int main(int strs, char** str) {
 	//执行平台功能
 	//string run_scipt;
 	//system(run_scipt.c_str());
-	cout << "SUCCESS "<<num;
+	cout << "SUCCESS "<<num<< " "<<fileout<<" "<<filepath;
 	return 0;
 }
