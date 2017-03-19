c8ec36ab7c2b58a8a1f64361973832cfbd522d07
slh0302 <928369573@qq.com>
2017-03-19 21:09:09 +0800

    changes

--- dataJs/index.js
+++ dataJs/index.js
@@ -1,3 +1,17 @@
 /**
  * Created by Su on 2017/3/19.
  */
+$(document).ready(function () {
+    $.get('./php/getDatabaseInfo.php',function (data) {
+        //db_file
+        var $add_data=$('#add-data');
+        if(data['msg']=="SUCCESS"){
+            $("#data").attr("data-to",data['data'].length);
+            var count=0;
+            $.each(data['data'],function(n,value) {
+                count+=parseInt(value['count']);
+            });
+            $("#count").attr("data-to",count);
+        }
+    });
+});
