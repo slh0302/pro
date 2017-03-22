800e14aa16c5566e279ca5a0fc9aef9c38ad4f82
slh <slh@bupt.edu.cn>
2017-03-22 10:58:23 +0800

    change

--- run/search/DoDetect.sh
+++ run/search/DoDetect.sh
@@ -2,7 +2,7 @@
 export PYTHONPATH=$PYTHONPATH:/home/xieqiang/Documents/Code/Detection/py-faster-rcnn-master/lib
 export PYTHONPATH=$PYTHONPATH:/home/xieqiang/Documents/Code/Detection/py-faster-rcnn-master/caffe-fast-rcnn/python
 cd /home/slh/Vechile_Search/libfaster_rcnn_cpp
-./bin/DoIndex -d $1 GPU 0
+./bin/DoIndex -d $1
 #/home/slh/data_model_1200_1080000 1080000  $1 1 1 128 GPU 0
 
 
