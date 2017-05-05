cbcffa66ded524309846b361795fc070b9e4734d
slh <slh@bupt.edu.cn>
2017-05-05 14:58:49 +0800

    changes

--- run/search/DoSearch.sh
+++ run/search/DoSearch.sh
@@ -2,7 +2,7 @@
 
 cd /home/slh/Vechile_Search/libfaster_rcnn_cpp
 #./bin/DoIndex -c /home/slh/data_wendeng_10800000_model_7000 1080000  $1 1 1 128 GPU 2
-./bin/DoIndex -s /home/slh/list.file 10000000 $1 1 1 150 GPU 0
+./bin/DoIndex -s /home/slh/list.file 10000000 $1 1 1 128 GPU 1
 
 # 1080000 test
 #./bin/DoIndex -c /home/slh/data_model_1200_1080000 1080000  $1 1 1 128 GPU 1
